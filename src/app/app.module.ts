96ed1f04ca967458c2e06c5251206ccfc158a795
Barbarakmenezes <barbarakerolaine@hotmail.com>
2022-12-03 01:58:37 -0300

    componente-inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './header/header.component';
+import { InicialComponent } from './inicial/inicial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    InicialComponent
   ],
   imports: [
     BrowserModule,
